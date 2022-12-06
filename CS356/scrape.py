0f1ba49fad6e99742898caf9633d75a1dc7fe3ca
Joel Ramirez <46511700+joelramirezjunior@users.noreply.github.com>
2022-12-06 13:47:38 -0800

    modified to grab everything

--- CS356/scrape.py
+++ CS356/scrape.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup 
+from urllib.parse import urlparse
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from urllib.request import urlopen
@@ -11,7 +12,8 @@ from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
 import glob, os
 
-TRUE_DIRECTORY = "urls/*urls" # "urls/test_urls" #
+
+TRUE_DIRECTORY =  "urls/*urls" # "urls/test_urls" #
 
 
 state = "return document.readyState"
@@ -23,20 +25,26 @@ def grab_html(url):
    options = webdriver.ChromeOptions();
    options.add_argument('headless');
    options.add_argument('window-size=1200x600');
-   browser = webdriver.Chrome(options=options)
+   try:
+      browser = webdriver.Chrome(options=options)
+   except:
+      print("Couldn't open Chrome")
+      return "hello"
+
    try:
       browser.get(url)
    except:
       print(f"URLS: {url}")
-   try:z
+   try:
       WebDriverWait(browser, 3).until(lambda d: d.execute_script(state) == "complete")
+   except:
+       print(f"TIMEOUT: {url}")
    finally: 
       html = browser.page_source
       browser.quit()
 
    a = html.split('"')
    html = " ".join(a)
-   lock = Lock()
    with lock:
       print(f'DONE: {url}\n')
 
@@ -45,6 +53,7 @@ def grab_html(url):
 def find_links(url):
    html = grab_html(url)
    urls = re.findall("(?P<url>https?://[^\s]+)", html)
+   urls.append(url)
    return urls
 
 def get_all_urls(name): #file_name: urls/bank_urls
@@ -57,15 +66,23 @@ def slice_per(source, step):
 def load_current_links(file_name): #file_name: urls/bank_urls
    list_of_urls = get_all_urls(file_name) #list of urls
    threads = list()
-   for urls in slice_per(list_of_urls, 10):
+   for urls in slice_per(list_of_urls, 5):
       with ThreadPoolExecutor(max_workers = 20) as executor:
          print(f"LOAD SIZE {len(urls)}")
          print(urls)
-         results = executor.map(find_links, urls)
-      f = open(f"found/{file_name.split('/')[1]}_links.txt", "a")
+         results = executor.map(find_links, urls, timeout=5)
+      f = open(f"found_no_repeating_domain/{file_name.split('/')[1]}_links.txt", "a")
       for result in results:
+         if result == []: continue
+         last_url = result[-1] 
          for url in result:
-               f.write(f"{url}\n")
+            try:
+               search_domain = urlparse(url).hostname
+            except ValueError as err:
+               print(f'{err}: {url}')
+               continue
+            if  search_domain is None or  search_domain in last_url: continue
+            f.write(f"{url}\n")
       print('First Write Finished')
       f.close()
 
